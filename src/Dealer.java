1d418217a53ffa47ed80292fb0b0fc661723ce25
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-18 12:30:06 -0600

    Reworked Dealer Class

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,5 +1,20 @@
 public class Dealer extends Player {
 
+  private Card hiddenCard;
+
+  public Card getHiddenCard() {
+    return hiddenCard;
+  }
+
+  public void setHiddenCard(Card card) {
+    hiddenCard = card;
+  }
+
+  public void revealHiddenCard() {
+    addToHand(hiddenCard);
+    hiddenCard = null;
+  }
+
   public boolean shouldDrawCard() {
     if (getHandValue() < 17) {
       return true;
